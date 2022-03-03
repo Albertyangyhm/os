37993dc34154636f2b67a81abf52f8b4375928b8
albert <hanmingy@middlebury.edu>
2022-03-03 09:28:58 -0500

    dtrace

--- ls.d
+++ ls.d
@@ -1,2 +1,22 @@
-iiii
+/*ls.d*/
+
+syscall:::entry
+
+/execname == "ls"/
+
+{
+	printf("%s(%d, %d, %d, %d, %d, %d)", stringof(execname), arg0, arg1, arg2, arg3, arg4, arg5);
+
+
+}
+syscall:::return
+
+/execname == "ls"/
+
+{
+	printf("\t\t = %d", arg1);
+	printf("\n");
+
+
+}
 
